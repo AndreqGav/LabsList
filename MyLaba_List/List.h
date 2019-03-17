fd66b30420ee055a63bf7f7d7167b3fb3702aa65
Andreq.Gav <Andreq.Gav@mail.ru>
2019-03-18 00:39:05 +0300

    ООП_3

--- MyLaba_List/List.h
+++ MyLaba_List/List.h
@@ -4,29 +4,207 @@
 
 using namespace std;
 
+template<class T>
 class List {
 
 private:
 	struct Node
 	{
-		int value;
+		T value;
 		Node *next;
 	};
 
 	Node *first = NULL;
-	int zero = 0;
+	T zero = 0;
 
-	int & GetSetIndex(int);
+	T & GetSetIndex(int);
 
 public:
 
-	void add(int);
+	void add(T);
 	void print();
 	int length();
-	int value(int);
+	T value(int);
 	void remove(int);
 
-	int &operator [] (int);
+	T &operator [] (int);
 	void sort(bool);
-	List sorted(bool);
+	List<T> sorted(bool);
 };
+
+template<class T>
+void List<T>::add(T value)
+{
+	Node *pv = new Node;
+	pv->value = value;
+	pv->next = NULL;
+
+	if (first == NULL)
+	{
+		first = pv;
+	}
+	else
+	{
+		Node *cur;
+		cur = first;
+
+		while (cur->next != NULL)
+		{
+			cur = cur->next;
+		}
+		cur->next = pv;
+	}
+}
+
+template<class T>
+void List<T>::print()
+{
+	if (first == NULL) {
+		cout << "empty" << endl;
+		return;
+	}
+	Node *cur = first;
+
+	while (cur != NULL)
+	{
+		cout << cur->value << ' ';
+		cur = cur->next;
+	}
+	cout << endl;
+}
+
+template<class T>
+int List<T>::length()
+{
+	if (first == NULL)
+		return 0;
+
+	Node *cur = first;
+	int i = 1;
+	while (cur->next != NULL)
+	{
+		++i;
+		cur = cur->next;
+	}
+	return i;
+}
+
+template<class T>
+T List<T>::value(int index)
+{
+	Node *cur;
+	if (index > (length() - 1) || index < 0)
+	{
+		cerr << "Индекс вышел за грани списка \n";
+		return 0;
+	}
+	else
+	{
+		cur = first;
+		int i = 0;
+		while (i < index)
+		{
+			cur = cur->next;
+			++i;
+		}
+	}
+	return cur->value;
+}
+
+template<class T>
+void List<T>::remove(int index)
+{
+	if (first == NULL)
+	{
+		cerr << "Список пуст!";
+		return;
+	}
+	else
+	{
+		if (index > (length() - 1) || index < 0)
+		{
+			cerr << "Не найдет элемент с заданным индексом\n";
+			return;
+		}
+		Node *cur = first;
+		Node *prev = NULL;
+		int i = 0;
+
+		while (i != index)
+		{
+			prev = cur;
+			cur = cur->next;
+			++i;
+		}
+		if (prev != NULL) {
+			prev->next = cur->next;
+		}
+		else
+		{
+			first = cur->next;
+		}
+		delete cur;
+	}
+}
+
+template<class T>
+T & List<T>::GetSetIndex(int index)
+{
+	zero = 0;
+
+	if (index > (length() - 1) || index < 0)
+	{
+		cerr << "Индекс вышел за грани списка \n";
+	}
+	else
+	{
+		int i = 0;
+		Node *pv = first;
+		while (i != index)
+		{
+			pv = pv->next;
+			++i;
+		}
+		return pv->value;
+	}
+	return zero;
+}
+
+template<class T>
+T & List<T>:: operator[](int index)
+{
+	return GetSetIndex(index);
+}
+
+template<class T>
+void List<T>::sort(bool ascending)
+{
+	int len = length();
+
+	Node *cur_i, *cur_j;
+	int i, j;
+	for (cur_i = first, i = 0; i < len - 1; cur_i = cur_i->next, i++)
+	{
+		for (cur_j = first, j = 0; j < len - i - 1; cur_j = cur_j->next, j++)
+		{
+			int a = cur_j->value, b = cur_j->next->value;
+
+			if (a > b && ascending || a < b && !ascending)
+			{
+				swap(cur_j->value, cur_j->next->value);
+			}
+		}
+	}
+}
+
+template<class T>
+List<T> List<T>::sorted(bool ascending)
+{
+	List newList;
+	for (int i = 0; i < length(); ++i)
+	{
+		newList.add(value(i));
+	}
+	newList.sort(ascending);
+	return newList;
+}
--- MyLaba_List/MyLaba_List.cpp
+++ MyLaba_List/MyLaba_List.cpp
@@ -1,43 +1,44 @@
 #include "stdafx.h"
 #include "List.h"
+#include <ctime>
 
 using namespace std;
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	List amm;
 
-	for (int i = 0; i < 5; ++i)
-		amm.add(rand() % 10 - 2);
-	cout << "amm: ";
-	amm.print();
-	cout << "amm[1] = 3" << endl;
-	cout << "amm[0] = 0" << endl;
-	amm[1] = 3;
-	amm[0] = 0;
-	cout << "amm: ";
-	amm.print();
-	cout << "amm[5] = 2" << endl;
-	amm[5] = 2;
+	List<float> amm;
 
-	cout << "amm[4]: " << amm[4] << endl;
-	cout << "amm[7]: " << amm[7] << endl;
+	srand(time(0));
+
+	for (int i = 0; i < 10; i++)
+	{
+		float fl = 0.0, up = 0.0, down = 0.0;
+
+		up = rand() % 9 + 1;
+		down = rand() % 9 + 1;
+		fl = up / down;
+		amm.add(fl);
+
+	}
+	cout << "amm:";
+	amm.print();
+	cout << "---------------------------------------" << endl;
+
+	cout << "fkn = amm.sorted(true)" << endl;
+	List<float> fkn = amm.sorted(true);
+	cout << "amm.add(0.01)" << endl;
+	amm.add(0.01);
+	cout << "fkn:";
+	fkn.print();
 
 	cout << "amm.sort(false)" << endl;
 	amm.sort(false);
-	cout << "amm: ";
+	cout << "amm:";
 	amm.print();
 
-	cout << " - - - - - - - - - -" << endl;
-
-	cout << "fkn = amm.sorted(true)";
-	List fkn = amm.sorted(true);
-	cout << "fkn: ";
-	fkn.print();
-	cout << "--------------------" << endl;
-	cout << "amm: ";
-	amm.print();
+	cout << "amm.value(100)" << amm.value(100) << endl;
 
 	system("pause");
 	return 0;
